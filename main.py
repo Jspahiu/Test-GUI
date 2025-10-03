889cef0521f023e3873e39cc5fb093cd88f925a2
Jspahiu <121984809+Jspahiu@users.noreply.github.com>
2025-10-03 00:07:13 +0000

    Your commit message

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.binary_location = "/usr/bin/chromium-browser"  # chromium path
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 def search_google(query, headless=False):
     options = webdriver.ChromeOptions()
